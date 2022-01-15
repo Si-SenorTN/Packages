a4ebbb83ac3ab2ad8d25eea32d1a20f73dca7e31
Luca F <imigin18090@gmail.com>
2022-01-14 19:17:53 -0500

    added config

---
+++ src/Config/doc.md
@@ -0,0 +1,24 @@
+# Config
+
+## Usage
+```lua
+local Config = require(somewhere.Config)
+
+local keybinds = Config.new({
+	Reload = Enum.KeyCode.R
+})
+
+keybinds:Observe("Reload", function(newValue, trove)
+	trove:BindAction("ReloadAction", function(_, inputState)
+		if inputState == Enum.InputState.Begin then
+			print("Pressing Reload")
+		end
+	end, newValue)
+end)
+
+keybinds:SetKey("Reload", Enum.KeyCode.R) -- nothing happens
+
+keybinds:SetKey("Reload", Enum.KeyCode.T) -- observed state reiterates, trove cleans and code runs back over
+
+keybinds:SetKey("Reload", Config.DeleteToken) -- deletes the key, cleans the trove, does not reiterate observed state
+```
--- src/Config/init.lua
+++ src/Config/init.lua
@@ -1,11 +1,13 @@
 local Trove = require(script.Parent.Trove)
 local Symbol = require(script.Parent.Symbol)
 
+local ERR_NO_SUBTABLES = "Cannot include subtables within State"
 local ERR_NOT_DICTIONARY = "State must be a dictionary"
 
 local State = Symbol.named("State")
 local Observers = Symbol.named("Observers")
 local DeleteToken = Symbol.named("DeleteToken")
+local InternalTrove = Symbol.named("InternalTrove")
 
 local Config = {}
 Config.prototype = {}
@@ -14,16 +16,32 @@ Config.DeleteToken = DeleteToken
 
 local globalConfigs = {}
 
+local function assertNoneShallow(t: {any}, typeToDisclude: any, msg: string)
+	for _, value in pairs(t) do
+		local dataType = typeof(value)
+		assert(dataType ~= typeToDisclude, msg)
+	end
+	return t
+end
+
 function Config.new(initialState: {[string]: any})
 	assert(type(initialState) == "table", ERR_NOT_DICTIONARY)
 
 	local state = initialState or {}
+	local trove = Trove.new()
 
 	local metatable = { __index = Config.prototype }
 	local self = setmetatable({
 		[State] = state,
-		[Observers] = {}
+		[Observers] = {},
+
+		[InternalTrove] = trove
 	}, metatable)
+
+	trove:Add(function()
+		table.clear(self)
+		setmetatable(self, nil)
+	end)
 
 	return self
 end
@@ -40,14 +58,85 @@ end
 
 function Config.prototype:Set(state)
 	assert(type(state) == "table", ERR_NOT_DICTIONARY)
-	-- override our entire state, check our observers. if theyre watching for a certain key AND its been changed, then do the
-	-- necessary update. if there was no change or key was removed, do nothing to it
+	assertNoneShallow(state, "table", ERR_NO_SUBTABLES)
 
+	local currentState = self[State]
 	local observers = self[Observers]
 
 	for name, data in pairs(observers) do
+		local currentValue, newValue = currentState[name], state[name]
+		if newValue and currentValue ~= newValue then
+			local trove = data._trove
+			trove:Destroy()
 
+			if newValue == DeleteToken then
+				self[State][name] = nil
+				observers[name] = nil
+
+				continue
+			end
+
+			local handler = data._handler
+			handler(newValue, trove)
+		end
 	end
+
+	self[State] = state
+end
+
+function Config.prototype:SetKey(key: string, newValue)
+	assert(typeof(newValue) == "table", ERR_NO_SUBTABLES)
+
+	local state = self[State]
+	local currentValue = state[key]
+
+	if currentValue and currentValue ~= newValue then
+		local observers = self[Observers]
+		local data = observers[key]
+
+		if data then
+			local trove = data._trove
+
+			if newValue == DeleteToken then
+				self[State][key] = nil
+				observers[key] = nil
+				self[InternalTrove]:Remove(trove)
+
+				return
+			end
+			trove:Destroy()
+
+			local handler = data._handler
+			handler(newValue, trove)
+		end
+	end
+
+	self[State][key] = newValue
+end
+
+function Config.prototype:Observe(key: string, observer)
+	local observers = self[Observers]
+	local existing = observers[key]
+
+	if existing then
+		local trove = existing._trove
+		trove:Destroy()
+	end
+
+	local trove = self[InternalTrove]:Construct(Trove)
+	observers[key] = {
+		_trove = trove,
+		_handler = observer
+	}
+
+	local currentState = self[State][key]
+	if currentState then
+		observer(currentState, trove)
+	end
+end
+
+function Config.prototype:Destroy()
+	self[InternalTrove]:Destroy()
 end
 
 return Config
